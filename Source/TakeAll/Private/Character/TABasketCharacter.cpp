7417764e626775ba075b08ab5846aecd8af37108
Denis <deskatihonov@gmail.com>
2024-12-07 14:07:24 +0300

    Rename and replace character class.

---
+++ Source/TakeAll/Private/Character/TABasketCharacter.cpp
@@ -0,0 +1,88 @@
+// Take All.haser inc.All Rights Reserved.
+
+#include "Character/TABasketCharacter.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Engine/SkeletalMesh.h"
+#include "Components/CapsuleComponent.h"
+#include "Trash/FA_Trash.h"
+
+ATABasketCharacter::ATABasketCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+
+    GetCharacterMovement()->MaxWalkSpeed = 500.f;
+
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
+    CameraBoom->TargetArmLength = 400.0f;
+    CameraBoom->bUsePawnControlRotation = true;
+
+    GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &ATABasketCharacter::OnBeginTrashOverlap);
+
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+    FollowCamera->bUsePawnControlRotation = false;
+
+    StaticMeshTop = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshTop"));
+    StaticMeshTop->SetupAttachment(GetMesh());
+
+    StaticMeshDown = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshDown"));
+    StaticMeshDown->SetupAttachment(GetMesh());
+}
+
+void ATABasketCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+    {
+        if (UEnhancedInputLocalPlayerSubsystem* Subsystem =
+            ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+        {
+            Subsystem->AddMappingContext(InputMapping, 0);
+        }
+    }
+}
+
+void ATABasketCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ATABasketCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+    if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        EnhancedInputComponent->BindAction(MoveRight, ETriggerEvent::Triggered, this, &ATABasketCharacter::OnMoveRight);
+    }
+}
+
+void ATABasketCharacter::OnMoveRight(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
+
+    if (Controller != nullptr)
+    {
+        const FRotator Rotation = Controller->GetControlRotation();
+        const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+        const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+        AddMovementInput(RightDirection, MovementVector.X);
+    }
+}
+
+void ATABasketCharacter::OnBeginTrashOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    Score++;
+
+    if (AFA_Trash* Trash = Cast<AFA_Trash>(OtherActor))
+    {
+        Trash->Destroy();
+    }
+}
---
+++ Source/TakeAll/Public/Character/TABasketCharacter.h
@@ -0,0 +1,60 @@
+// Take All.haser inc.All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "TABasketCharacter.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class TAKEALL_API ATABasketCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    ATABasketCharacter();
+
+protected:
+    virtual void BeginPlay() override;
+
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    class USpringArmComponent* CameraBoom;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+    class UCameraComponent* FollowCamera;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    class UStaticMeshComponent* StaticMeshTop;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    class UStaticMeshComponent* StaticMeshDown;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "BusketInput")
+    UInputAction* MoveRight;
+
+    UPROPERTY(EditDefaultsOnly, Category = "BusketInput")
+    UInputMappingContext* InputMapping;
+
+protected:
+    UFUNCTION()
+    void OnBeginTrashOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+        const FHitResult& SweepResult);
+
+public:
+    virtual void Tick(float DeltaTime) override;
+
+    int32 GetScore() const { return Score; };
+
+private:
+    void OnMoveRight(const FInputActionValue& Value);
+    int32 Score = 0;
+};
